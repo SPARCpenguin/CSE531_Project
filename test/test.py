11c3a4fbd3defa2ff3a3c13b5c31fa7a78bcf67a
SPARCpenguin <sean.gatenby@asu.edu>
2018-12-04 20:12:44 -0700

    Updated python script one last time

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 import paramiko
 import os
 import time
+from timeit import default_timer as timer
 from cProfile import run
 import cmd
 import socket
@@ -26,10 +27,11 @@ clusterServers = "server_1:5254 server_2:5254 server_3:5254 server_4:5254 server
 goldenFiles = ["Space: the final frontier. These are the voyages of the starship Enterprise. Its continuing mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before","A long time ago in a galaxy far, far away... STAR WARS It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy"]
 
 class clientThread (threading.Thread):
-    def __init__(self, threadID, clientName, sshSession, scriptName, runIndex):
+    def __init__(self, threadID, clientName, serverName, sshSession, scriptName, runIndex):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.clientName = clientName
+        self.serverName = serverName
         self.sshSession = sshSession
         self.scriptName = scriptName
         self.runIndex = runIndex
@@ -37,7 +39,7 @@ class clientThread (threading.Thread):
         # Open an SSH session and write commands to stdin
         stdin, stdout, stderr = self.sshSession.exec_command("bash")
         stdin.write("cd " + testPath + "\n")
-        cmd = "../simpleFileLockService/bin/" + clientBinaryName + " " + socket.gethostbyname("server_5") + " " + self.clientName + " 1 " + serverPort + " " + self.scriptName + " > log/" + str(self.runIndex) + "_" + self.clientName + "_cmd.log 2>&1\n"
+        cmd = "../simpleFileLockService/bin/" + clientBinaryName + " " + socket.gethostbyname(self.serverName) + " " + self.clientName + " 1 " + serverPort + " " + self.scriptName + " > log/" + str(self.runIndex) + "_" + self.clientName + "_cmd.log 2>&1\n"
         print cmd
         stdin.write(cmd)
         stdin.write("exit\n")
@@ -65,7 +67,7 @@ def runTest(numClients, numServers, scripts, numFailures, runIndex):
         clientSSH[i].set_missing_host_key_policy(paramiko.AutoAddPolicy())
         clientSSH[i].connect(hostname=host, username="client", pkey=sshKey)
         
-        stdin, stdout, stderr = clientSSH[i].exec_command("killall -9 " + clientBinaryName)
+        stdin, stdout, stderr = clientSSH[i].exec_command("killall " + clientBinaryName)
         stdout.channel.exit_status_ready()
 
     # Loop through servers and kill any previously running processes
@@ -77,33 +79,22 @@ def runTest(numClients, numServers, scripts, numFailures, runIndex):
         serverSSH[i].connect(hostname=host, username="server", pkey=sshKey)
         
         # Need to kill log cabin, remove storage directory, and kill FT SFL service
-        if numServers > 1:
-            # The log cabin service is running on every server, so kill it
+        stdin, stdout, stderr = serverSSH[i].exec_command("bash")
+        stdin.write("killall " + logCabinBinaryName + "\n")
+        stdin.write("killall " + ft_serverBinaryName + "\n")
+        stdin.write("killall " + serverBinaryName + "\n")
+        stdin.write("exit\n")
+        stdout.channel.exit_status_ready()
+        time.sleep(1)
+        
+        if i == (numServers - 1):
             stdin, stdout, stderr = serverSSH[i].exec_command("bash")
-            stdin.write("killall -9 " + logCabinBinaryName + "\n")
-
-            if i == (numServers - 1):
-                # We only need to clean up the storage directory once
-                # and since the filesystem is shared, we'll just remove the
-                # directory after connecting to the last server
-                stdin.write("rm -rf " + testPath + "/storage\n")
-                # The simple file locking service only needs to run on one
-                # machine, so the last server will be used for this as wel.
-                # kill any running copies
-                stdin.write("killall -9 " + ft_serverBinaryName + "\n")
-
-            stdin.write("exit\n")
-            stdout.channel.exit_status_ready()
-        # Just need to kill SFL service
-        else:
-            stdin, stdout, stderr = serverSSH[i].exec_command("bash")
-            stdin.write("killall -9 " + serverBinaryName + "\n")
+            stdin.write("rm -rf " + testPath + "/storage\n")
             stdin.write("rm -rf " + testPath + "/*.txt\n")
             stdin.write("rm -rf " + testPath + "/incarnation*\n")
             stdin.write("exit\n")
             stdout.channel.exit_status_ready()
-
-    time.sleep(1)
+            time.sleep(1)
 
     # Loop through servers and start up logcabin cluster
     for i in range(0,numServers):
@@ -157,17 +148,12 @@ def runTest(numClients, numServers, scripts, numFailures, runIndex):
             # Start up Simple File Locking Service
             stdin, stdout, stderr = serverSSH[i].exec_command("bash")
             stdin.write("cd " + testPath + "\n")
-            stdin.write("nohup ../simpleFileLockService/bin/" + serverBinaryName + " 9001 > log/" + str(runIndex) + "_" + host + "_simplefilelockservice.log 2>&1 &\n")
+            cmd = "nohup ../simpleFileLockService/bin/" + serverBinaryName + " 9001 > log/" + str(runIndex) + "_" + host + "_simplefilelockservice.log 2>&1 &\n"
+            print cmd
+            stdin.write(cmd)
             stdin.write("exit\n")
             stdout.channel.exit_status_ready()
             time.sleep(.5)
- 
-    # Loop through the client list and kick off the client threads
-    for i in range(0,numClients):
-        host = clientPrefix + str(i + 1)
-        thread[i] = clientThread(i, host, clientSSH[i], scripts[i], runIndex)
-        thread[i].start()
-        print "Client " + str(i) + " started"
          
     # If a failure is requested, trigger the failure(s)
     if numFailures > 0:
@@ -179,52 +165,68 @@ def runTest(numClients, numServers, scripts, numFailures, runIndex):
         # Kill requested number of servers
         for i in range(0,numFailures):      
             if numServers > 1:
-                stdin, stdout, stderr = serverSSH[i].exec_command("killall -9 " + logCabinBinaryName)
+                stdin, stdout, stderr = serverSSH[i].exec_command("killall " + logCabinBinaryName)
                 stdout.channel.exit_status_ready()
+                # Wait for logcabin to reconfigure without that server
+                time.sleep(1)
+                
             else:
-                stdin, stdout, stderr = serverSSH[i].exec_command("killall -9 " + serverBinaryName)
+                stdin, stdout, stderr = serverSSH[i].exec_command("killall " + serverBinaryName)
                 stdout.channel.exit_status_ready()
- 
+
+    # Loop through the client list and kick off the client threads
+    for i in range(0,numClients):
+        clientName = clientPrefix + str(i + 1)
+        serverName = serverPrefix + str(numServers)
+        thread[i] = clientThread(i, clientName, serverName, clientSSH[i], scripts[i], runIndex)
+        thread[i].start()
+        print "Client " + str(i) + " started"
+
+    if numFailures > 0:
         # Need to kill clients as well so the script won't hang on the "join" call
+        time.sleep(5)
+        
         for i in range(0,numClients):      
-                stdin, stdout, stderr = clientSSH[i].exec_command("killall -9 " + clientBinaryName)
-                stdout.channel.exit_status_ready()
- 
+            stdin, stdout, stderr = clientSSH[i].exec_command("killall " + clientBinaryName)
+            stdout.channel.exit_status_ready()
+
     print "waiting to join"
     # Wait for client threads to exit
     for i in range(0,numClients):
         thread[i].join()
-         
+          
     print "done!"
      
     testPassed = True
      
+    time.sleep(2)
+    
     if numServers > 1:
         for i in range(0,numClients):
             outfile = "client_" + str(i+1) + ":BestSpaceOpera.txt"
             stdin, stdout, stderr = clientSSH[i].exec_command("bash")
             stdin.write("cd " + testPath + "\n")
-            cmd = "../logcabin/build/Examples/TreeOps --cluster=" + clusterServersNoSpace + " read " + outfile + " --verbosity=SILENT\n"
+            cmd = "../logcabin/build/Examples/TreeOps --timeout=5 --cluster=" + clusterServersNoSpace + " read " + outfile + " --verbosity=SILENT\n"
             print cmd
             stdin.write(cmd)
             stdin.write("exit\n")
             stdout.channel.exit_status_ready()
-            time.sleep(1)
             
             output = stdout.read()
             
-            print output
-            print goldenFiles[i]
+            print output.rstrip()
+            print goldenFiles[i].rstrip()
             
-            if output != goldenFiles[i]:
+            if output.rstrip() != goldenFiles[i].rstrip():
                 testPassed = False
-    else:
-        time.sleep(2)
-     
+    else:     
         for i in range(0,numClients):
             outFile = "/nfsShare/test/client_" + str(i+1) + ':BestSpaceOpera.txt'
-            f = open(outFile, 'r')
-            output = f.read()
+            try:
+                f = open(outFile, 'r')
+                output = f.read()
+            except:
+                output = ""
 
             if output != goldenFiles[i]:
                 testPassed = False
@@ -240,20 +242,57 @@ def runTest(numClients, numServers, scripts, numFailures, runIndex):
 
 scripts = ["StarTrek.cmd", "StarWars.cmd"]
 
-logFile = "/nfsShare/test/testStatus.out"
+logFile = "/nfsShare/test/testStatus.csv"
 
 f = open(logFile, 'w')
 
-for i in range(0,100l):
-    status = runTest(2,5, scripts, 0, i)
-    value = ""
-    
-    if status == True:
-        value = "PASSED\n"
-    else:
-        value = "FAILED\n"
-    
-    f.write("Run " + str(i) + ": " + value)
+numRuns = 1
+
+start = timer()
+
+f.write("1 server 0 failures, ")
+for i in range(0,numRuns):
+    status = runTest(2,1, scripts, 0, i)
+    f.write(str(int(status)) + ", ")
     f.flush()
+f.write("\n")
+  
+f.write("1 server 1 failures, ")
+for i in range(0,numRuns):
+    status = runTest(2,1, scripts, 1, i)
+    f.write(str(int(status)) + ", ")
+    f.flush()
+f.write("\n")
+  
+f.write("5 server 0 failures, ")
+for i in range(0,numRuns):
+    status = runTest(2,5, scripts, 0, i)
+    f.write(str(int(status)) + ", ")
+    f.flush()
+f.write("\n")
     
+f.write("5 server 1 failures, ")
+for i in range(0,numRuns):
+    status = runTest(2,5, scripts, 1, i)
+    f.write(str(int(status)) + ", ")
+    f.flush()
+f.write("\n")
+    
+f.write("5 server 2 failures, ")
+for i in range(0,numRuns):
+    status = runTest(2,5, scripts, 2, i)
+    f.write(str(int(status)) + ", ")
+    f.flush()
+f.write("\n")
+   
+f.write("5 server 3 failures, ")
+for i in range(0,numRuns):
+    status = runTest(2,5, scripts, 3, i)
+    f.write(str(int(status)) + ", ")
+    f.flush()
+f.write("\n")
 f.close()
+
+end = timer()
+
+print(end - start)
